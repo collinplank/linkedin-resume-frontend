2303c681b56ce3306feba83484755814d158ff27
Collin Plank <collin.plank@gmail.com>
2025-01-03 14:38:09 -0500

    Updated Modal

--- src/UsersIndex.jsx
+++ src/UsersIndex.jsx
@@ -1,14 +1,10 @@
-
 export function UsersIndex({ users, onShow }) {
-
-
   return (
     <div>
       <h1>Profiles</h1>
       {users.map((user) => (
         <div key={user.id}>
-          <h2>{user.first_name}</h2>
-          <h2>{user.last_name}</h2>
+          <h2>{user.first_name + " " + user.last_name}</h2>
           <p>{user.email}</p>
           <p>{user.phone_number}</p>
           <p>{user.short_bio}</p>
@@ -17,8 +13,8 @@ export function UsersIndex({ users, onShow }) {
           <p>{user.website_url}</p>
           <p>{user.online_resume_url}</p>
           <p>{user.github_url}</p>
-          <p>{user.photo}</p>
-          <button onClick={() => onShow(user)}>More info</button>
+          <img src={user.photo} />
+          <button onClick={() => onShow(user)}>More Info</button>
         </div>
       ))}
     </div>
